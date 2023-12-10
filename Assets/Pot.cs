44d2f917466e27be1531d8da71586744f54d67ff
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-12-10 15:20:14 +0200

    Pop Animation

--- Assets/Pot.cs
+++ Assets/Pot.cs
@@ -2,11 +2,29 @@ using UnityEngine;
 
 public class Pot : MonoBehaviour
 {
+   public float shrinkSpeed = 5;
+   public Vector3 rotateSpeed;
+   AudioSource source;
 
+   void Start()
+   {
+      source = GetComponent<AudioSource>();
+   }
+
+   void Update()
+   {
+      transform.Rotate(rotateSpeed * Time.deltaTime);
+
+      if (transform.localScale.x > 1f)
+      {
+         transform.localScale -= Vector3.one * shrinkSpeed * Time.deltaTime;
+      }
+   }
 
    void OnMouseDown()
    {
       GameManager.clicks++;
-      print("clicked");
+      transform.localScale = Vector3.one * 1.3f;
+      source.PlayOneShot(source.clip);
    }
 }
