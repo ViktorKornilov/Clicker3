8bee74438573fadbcb4eae929da6e71af7ec019e
vik <39262485+ViktorKornilov@users.noreply.github.com>
2023-12-10 14:58:29 +0200

    Worker Button

--- Assets/Worker.cs
+++ Assets/Worker.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Worker : MonoBehaviour
 {
@@ -10,15 +11,20 @@ public class Worker : MonoBehaviour
 
 	public TMP_Text countText;
 	public TMP_Text priceText;
+	public Button button;
 
 	void Update()
 	{
 		countText.text = count.ToString();
 		priceText.text = "price:" + price;
+
+		var canClick = GameManager.clicks >= price;
+		button.interactable = canClick;
 	}
 
 	void Start()
 	{
+		InvokeRepeating("Work",1f,1f);
 		Load();
 	}
 
@@ -27,17 +33,25 @@ public class Worker : MonoBehaviour
 		if (GameManager.clicks < price) return;
 
 		GameManager.clicks -= price;
+		price = (int)(price * 1.4f);
 		count++;
 		Save();
+	}
+
+	void Work()
+	{
+		GameManager.clicks += count * cps;
 	}
 
 	void Save()
 	{
 		PlayerPrefs.SetInt(name, count);
+		PlayerPrefs.SetInt(name + "price",price);
 	}
 
 	void Load()
 	{
 		count = PlayerPrefs.GetInt(name);
+		price = PlayerPrefs.GetInt(name + "price",price);
 	}
 }
