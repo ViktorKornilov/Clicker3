18ce61c877e8d69aab13951f91714e1fd8843009
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-11-04 17:16:57 +0200

    Click Timer

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,36 @@ public class GameManager : MonoBehaviour
 {
     public TMP_Text clicksText;
     public int clicks;
+    public int grandmas;
+    public int grandmaPrice;
+    public float clickTimer;
 
     void Update()
     {
+        // UPDATE UI
         clicksText.text = clicks.ToString();
+
+        // TIMER
+        clickTimer += Time.deltaTime;
+
+        if (clickTimer > 1f)
+        {
+            clicks += grandmas;
+            clickTimer = 0;
+        }
     }
 
     public void AddClick()
     {
         clicks++;
+    }
+
+    public void BuyGrandma()
+    {
+        if (clicks < grandmaPrice) return;
+
+        clicks -= grandmaPrice;
+        grandmas++;
+        grandmaPrice *= 2;
     }
 }
