2611f9f313577b3d19c767c0fdb0fbb5dfddeef9
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-11-10 14:58:45 +0200

    Save / Load

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,23 +8,50 @@ public class GameManager : MonoBehaviour
     public TMP_Text grandmaCountText;
     public TMP_Text clicksText;
     int grandmaCount;
+    int grandmaPrice;
 
+    public float clickTimer;
+
+    void Start()
+    {
+        Load();
+    }
+
+    void Update()
+    {
+        clickTimer += Time.deltaTime;
+        if (clickTimer >= 1)
+        {
+            AddClick();
+            clickTimer = 0;
+        }
+
+        clicksText.text = clicks.ToString();
+    }
+
+    void OnApplicationQuit()
+    {
+        Save();
+    }
+
+    void Save()
+    {
+        PlayerPrefs.SetInt("clicks", clicks);
+        PlayerPrefs.SetInt( "grandmaCount", grandmaCount);
+        PlayerPrefs.SetInt( "grandmaPrice", grandmaPrice);
+    }
+
+    void Load()
+    {
+        clicks = PlayerPrefs.GetInt("clicks");
+        grandmaCount = PlayerPrefs.GetInt("grandmaCount");
+        grandmaPrice = PlayerPrefs.GetInt("grandmaPrice");
+    }
 
 
     public void AddClick()
     {
         clicks++;
-        clicksText.text = clicks.ToString();
-    }
-
-    void Start()
-    {
-        clicks = PlayerPrefs.GetInt("clicks");
-    }
-
-    void OnApplicationQuit()
-    {
-        PlayerPrefs.SetInt("clicks", clicks);
     }
 
     public void BuyGrandma()
