980d33a8e717547db0b7e307eb585377adf4d2c2
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-11-17 14:12:59 +0200

    Cookie Animation

--- Assets/Scripts/Cookie.cs
+++ Assets/Scripts/Cookie.cs
@@ -5,8 +5,17 @@ public class Cookie : MonoBehaviour
 {
     public UnityEvent onClick;
 
+    void Update()
+    {
+        if (transform.localScale.x > 1f)
+        {
+            transform.localScale -= Vector3.one * 2 * Time.deltaTime;
+        }
+    }
+
     void OnMouseDown()
     {
+        transform.localScale = Vector3.one * 1.5f;
         onClick.Invoke();
     }
 }
